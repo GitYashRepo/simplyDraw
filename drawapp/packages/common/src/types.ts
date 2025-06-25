25b5361797b42a7e53d94fe86db5508e139f4710
yashrepogit <yas@cloudify.biz>
2025-06-25 17:08:44 +0530

    complete code

--- drawapp/packages/common/src/types.ts
+++ drawapp/packages/common/src/types.ts
@@ -1,18 +1,17 @@
-import { z } from 'zod';
+import {z} from "zod";
 
-export const CreateUserSchema = z.object({
-    username: z.string().min(1, "Username is required").max(30, "Username must be at most 20 characters long"),
-    password: z.string().min(4, "Password must be at least 6 characters long"),
+export const CreateUserSchema=z.object({
+    username: z.string().min(2).max(15),
+    password: z.string().min(4),
     name: z.string()
 })
 
-export const SignInSchema = z.object({
-    username: z.string().min(1, "Username is required").max(30, "Username must be at most 20 characters long"),
-    password: z.string().min(4, "Password must be at least 6 characters long"),
+export const SigninSchema=z.object({
+    username:z.string().min(2).max(15),
+    password: z.string().min(4),    
 })
 
-export const CreateRoomSchema = z.object({
-    name: z.string().min(1, "Room name is required").max(20, "Room name must be at most 30 characters long"),
-    description: z.string().optional(),
-    password: z.string().optional()
+export const CreateRoomSchema=z.object({
+    name: z.string().min(3).max(25)
 })
+
